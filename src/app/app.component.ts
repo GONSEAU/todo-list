5c858b333035d87ef14455237521b026a962341b
Gonseau Robin <robin.gonseau@gmail.com>
2021-01-20 17:06:18 +0100

    html todolist + ts etape 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+  public todoArray: Array<string> = [
+    'item 1',
+    'item 1',
+    'item 1',
+    'item 1',
+    'alors',
+
+  ];
+  public addItem($event: KeyboardEvent): void {
+    if ($event.code === 'Enter') {
+      const $input: HTMLInputElement|null = $event.target as HTMLInputElement;
+
+      if ($input !== null) {
+        console.log($input.value);
+      }
+      console.log('Enter pressed')
+    }
+  }
 }
